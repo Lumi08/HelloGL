aae22fa994b5c0b41a0653d468b5f1adf11ad8e3
Ryan Goldsmith <50884845+ryangoldsmith@users.noreply.github.com>
2020-04-17 17:06:19 +0100

    Displays Red Rectangle

--- HelloGL/HelloGL.cpp
+++ HelloGL/HelloGL.cpp
@@ -17,5 +17,13 @@ HelloGL::~HelloGL(void)
 
 void HelloGL::Display()
 {
-
+	glClear(GL_COLOR_BUFFER_BIT);
+	glBegin(GL_POLYGON);
+		glColor4f(1.0f, 0.0f, 0.0f, 0.0f);
+		glVertex2f(-0.75, 0.5);
+		glVertex2f(0.75, 0.5);
+		glVertex2f(0.75, -0.5);
+		glVertex2f(-0.75, -0.5);
+	glEnd();
+	glFlush();
 }
